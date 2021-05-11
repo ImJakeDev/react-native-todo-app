74a4001ebec4d8e58779f5afde508e4cac82eeb4
Jake Schaffer <imjakedev@gmail.com>
2021-05-11 10:10:57 -0500

    added createStackNavigator Stack to app with a HomeScreen component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import "react-native-gesture-handler";
 import * as React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>🦧 This is Jake's React Native Todo app!</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>🦧 This is Jake's React Native Todo app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
