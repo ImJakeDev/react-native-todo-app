8b3f422a7d05470de54a8ef5405de0392969cd04
Jake Schaffer <imjakedev@gmail.com>
2021-05-11 10:26:08 -0500

    added Navigation component and deconstructed the navigation logic out of App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import "react-native-gesture-handler";
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 
-function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>🦧 This is Jake's React Native Todo app!</Text>
-    </View>
-  );
-}
-
-function DetailsScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Details Screen 🦙</Text>
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
+import Navigation from "./components/Navigation";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Overview" }}
-        />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
+    <>
+      <Navigation/>
       <StatusBar style="auto" />
-    </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
