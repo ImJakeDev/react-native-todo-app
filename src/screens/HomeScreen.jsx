82b23d618c23dce727b0413826dafe4d1eb26f22
Jake Schaffer <imjakedev@gmail.com>
2021-05-17 12:01:59 -0500

    added ImageBackground component to test background images on home screen

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,9 +1,15 @@
 import * as React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { ImageBackground, StyleSheet, Text, View, Button } from "react-native";
+
+const image = {
+  // uri: "https://images.unsplash.com/photo-1621146027714-e8921770f8d0",
+  // uri: "https://images.unsplash.com/photo-1613058982958-49ce4bd14d1e",
+  uri: "https://images.unsplash.com/photo-1550114535-9661885b0cf9",
+};
 
 export default function HomeScreen({ navigation }) {
   return (
-    
+    <ImageBackground source={image} style={styles.image}>
       <View style={styles.container}>
         <Text>🦧 This is Jake's React Native Todo app!</Text>
         <Button
@@ -15,15 +21,20 @@ export default function HomeScreen({ navigation }) {
           onPress={() => navigation.navigate("TodoContext")}
         />
       </View>
-    
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    // backgroundColor: "#fff",
     alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
     justifyContent: "center",
   },
 });
