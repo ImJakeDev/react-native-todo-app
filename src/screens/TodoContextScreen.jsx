3ee98111c8bca8f1acd5178a9940d0fced8e8882
Jake Schaffer <imjakedev@gmail.com>
2021-05-18 13:19:38 -0500

    updated title to hero title

--- src/screens/TodoContextScreen.jsx
+++ src/screens/TodoContextScreen.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 import TodoListContext from "../components/TodoListContext";
 import TodoInputContext from "../components/TodoInputContext";
-import Title from "../components/Title";
+import HeroTitle from "../components/HeroTitle";
 import ScreenLayouts from "../components/ScreenLayouts";
 
 export default function TodoContext() {
   return (
     <ScreenLayouts image="mountain">
-      <Title>Contextual To-Do List ⛩</Title>
+      <HeroTitle>Contextual To-Do List ⛩</HeroTitle>
       <TodoInputContext placeholder={"Type a todo, then hit enter!"} />
       <TodoListContext />
     </ScreenLayouts>
