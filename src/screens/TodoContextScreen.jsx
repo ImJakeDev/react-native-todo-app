c9c362a0247c9a69e23de479a4989b56c27a1f60
Jake Schaffer <imjakedev@gmail.com>
2021-05-18 11:57:01 -0500

    updated eh two todo screens layouts

--- src/screens/TodoContextScreen.jsx
+++ src/screens/TodoContextScreen.jsx
@@ -1,20 +1,36 @@
 import React from "react";
-import { View } from "react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
 
 import TodoListContext from "../components/TodoListContext";
 import TodoInputContext from "../components/TodoInputContext";
 import Title from "../components/Title";
 
+const image = {
+  uri: "https://images.unsplash.com/photo-1613058982958-49ce4bd14d1e"
+};
+
 export default function TodoContext() {
   return (
-    <View>
-      <Title>Contextual To-Do List ⛩</Title>
-      
-        <TodoInputContext
-          placeholder={"Type a todo, then hit enter!"}
-        />
+    <ImageBackground source={image} style={styles.image}>
+      <View style={{ flex: 1, padding: 20 }}>
+        <Title>Contextual To-Do List ⛩</Title>
+        <TodoInputContext placeholder={"Type a todo, then hit enter!"} />
         <TodoListContext />
-      
-    </View>
+      </View>
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 6,
+    borderColor: "rgba(233, 236, 241, 0.5)",
+    borderWidth: 3,
+    padding: 20,
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+});
--- src/screens/TodoScreen.jsx
+++ src/screens/TodoScreen.jsx
@@ -1,25 +1,45 @@
 import React, { useReducer } from "react";
-import { View } from "react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
 
 import TodoList from '../components/TodoList';
 import TodoInput from "../components/TodoInput";
 import Title from "../components/Title";
 import { actionCreators, reducer, initialState } from "../reducers/todos";
 
+const image = {
+  uri: "https://images.unsplash.com/photo-1621146027714-e8921770f8d0"
+};
+
 export default function Todo({ navigation }) {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <View>
-      <Title>To-Do List 🌋</Title>
-      <TodoInput
-        placeholder={"Type a todo, then hit enter!"}
-        onSubmitEditing={(title) => dispatch(actionCreators.add(title))}
-      />
-      <TodoList
-        items={state.items}
-        onPressItem={(id) => dispatch(actionCreators.remove(id))}
-      />
-    </View>
+    <ImageBackground source={image} style={styles.image}>
+      <View style={{ flex: 1, padding: 20 }}>
+        <Title>To-Do List 🌋</Title>
+        <TodoInput
+          placeholder={"Type a todo, then hit enter!"}
+          onSubmitEditing={(title) => dispatch(actionCreators.add(title))}
+        />
+        <TodoList
+          items={state.items}
+          onPressItem={(id) => dispatch(actionCreators.remove(id))}
+        />
+      </View>
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 6,
+    borderColor: "rgba(233, 236, 241, 0.5)",
+    borderWidth: 3,
+    padding: 20,
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+});
